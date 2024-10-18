db8210871fa1c9053fb8e0f49b47aa444a9ffc37
Ksenia Razheva <pokatskayakseniya@gmail.com>
2024-10-18 18:15:42 +0300

    update app, add-form, list-item, list. add deletoin&addition employees step6

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -24,16 +24,17 @@ class EmployeesListItem extends Component {
 	};
 
 	render() {
-		const { name, salary } = this.props;
+		const { name, salary, onDelete } = this.props;
 		const { increase, rise } = this.state;
 
 		let classNames = "list-group-item d-flex justify-content-between";
 		if (increase) {
-			classNames += " increase ";
+			classNames += " increase";
 		}
 		if (rise) {
 			classNames += " like";
 		}
+
 		return (
 			<li className={classNames}>
 				<span className="list-group-item-label" onClick={this.onRise}>
@@ -53,7 +54,11 @@ class EmployeesListItem extends Component {
 						<i className="fas fa-cookie"></i>
 					</button>
 
-					<button type="button" className="btn-trash btn-sm ">
+					<button
+						type="button"
+						className="btn-trash btn-sm "
+						onClick={onDelete}
+					>
 						<i className="fas fa-trash"></i>
 					</button>
 					<i className="fas fa-star"></i>
